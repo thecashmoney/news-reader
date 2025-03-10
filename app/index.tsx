edff192380aaf48dfac1fa5af786a7b168535861
Elliott Jang <130790463+thecashmoney@users.noreply.github.com>
2025-03-10 13:01:20 -0700

    make text bigger; add in theming

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,42 @@
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Text, View, StyleSheet, useColorScheme } from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function Index() {
-  const insets = useSafeAreaInsets();
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        paddingTop: insets.top,
-      }}
-    >
-      <Text>News Reader</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={[styles.container, themeContainerStyle]}>
+        <Text style={[themeTextStyle, styles.titleText]} >News Reader</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  lightContainer: {
+    backgroundColor: '#A1CEDC',
+  },
+  darkContainer: {
+    backgroundColor: '#1D3D47',
+  },
+  lightThemeText: {
+    color: 'black',
+  },
+  darkThemeText: {
+    color: 'white',
+  },
+  titleText: {
+    paddingTop: 20,
+    fontWeight: 'bold',
+    fontSize: 40,
+  }
+});
